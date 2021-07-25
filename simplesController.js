bad8f12963e0d763c079de04afeb099af8e076db
Fernando Kiosa <fernandojoaokiosa90@gmail.com>
2021-07-24 22:50:05 -0300

    adicionado formulario

--- simplesController.js
+++ simplesController.js
@@ -2,8 +2,12 @@ var app = angular.module('app', []);
 
 app.controller('simpleController', function ($scope) {
     $scope.user = {
-        name: "Daniel",
+        name: "Fernando",
+        subname: "Kiosa",
+        address: "JD. Firenze Rua Milão Nº375a",
+        profession: "Programador",
         id: 1,
-        email: 'ino'
+        email: 'Fernandokiosa09@gmail.com'
+        
     }
 })
